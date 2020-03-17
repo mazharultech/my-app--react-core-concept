08aae85ebaeffcc8bbdbf50508dd2268e50834e6
mazharultech <mazharulislam807064@gmail.com>
2020-03-17 18:20:08 +0600

    persion2 added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
     occupation: 'Front web developer & WordPress Developer'
   }
 
+  var person2 = {
+    name: 'Tarep Rahman Khan',
+    occupation: 'Freelancer'
+  }
+
   var style = {
     color: 'red',
     backgroundColor: 'yellow'
@@ -21,6 +26,7 @@ function App() {
         <p>This my first react learning with Jhankar Mahbub</p>
         <h1>Name Heading: {persion.name + " " + persion.occupation + " " + 12 * 12}</h1>
         <h1 className="" style={style}>create react app</h1>
+        <p>SellerDashboard: {person2.name + ' ' + person2.occupation}</p>
       </header>
     </div>
   );
